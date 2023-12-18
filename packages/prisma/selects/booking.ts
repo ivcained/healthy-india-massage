8d6771900086661d191a07e3a08355264238f42a
Percy <percy@railway.app>
2023-12-18 18:04:16 +0000

    Initial Commit

---
+++ packages/prisma/selects/booking.ts
@@ -0,0 +1,12 @@
+import { Prisma } from "@prisma/client";
+
+export const bookingMinimalSelect = Prisma.validator<Prisma.BookingSelect>()({
+  id: true,
+  title: true,
+  description: true,
+  customInputs: true,
+  startTime: true,
+  endTime: true,
+  attendees: true,
+  metadata: true,
+});
